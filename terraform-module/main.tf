9995275bf9dbadec83aa0da00242239c87a2fbd2
rhysjtevans <rhys@rhysevans.co.uk>
2023-03-30 00:03:05 +0100

    added more fixes

--- terraform-module/main.tf
+++ terraform-module/main.tf
@@ -5,7 +5,7 @@ variable "k8s_secretrefname" {
   default = "gitlabber-env"
 }
 variable "gitlab_url" { }
-variable "gitlab_token" { }
+
 variable "s3_backup_bucket_name" { }
 
 variable "s3_backup_path" {
@@ -16,6 +16,24 @@ resource "tls_private_key" "clone_key" {
   algorithm = "ED25519"
 }
 
+resource "gitlab_personal_access_token" "main" {
+  user_id    = 2
+  name       = "Gitlabber-backup"
+  expires_at = "2026-03-14"
+
+  scopes = ["api"]
+}
+
+resource "gitlab_user_sshkey" "main" {
+  user_id    = 2
+  title      = "gitlabber"
+  key        = tls_private_key.clone_key.public_key_openssh
+
+  expires_at = "2016-01-21T00:00:00.000Z"
+}
+
+
+
 resource "kubernetes_secret" "conf" {
   metadata {
     name = var.k8s_secretrefname
@@ -24,7 +42,7 @@ resource "kubernetes_secret" "conf" {
 
   data = {
     GITLAB_URL = var.gitlab_url
-    GITLAB_TOKEN = var.gitlab_token
+    GITLAB_TOKEN = gitlab_personal_access_token.main.token
     S3_BACKUP_BUCKET_NAME = var.s3_backup_bucket_name
     S3_BACKUP_PATH = var.s3_backup_path
     SSH_PRIVATE_KEY = tls_private_key.clone_key.private_key_openssh
